0bc4bc594c7ef7de00563a32948879a33ed1cabd
Ashutosh Mohanty <94921263+ashutosh-mohanty05@users.noreply.github.com>
2024-06-01 00:33:45 +0530

    commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ASHU</div>
+          <div className="right">
+            <p>403A, NAYAPALLI, BBSR</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ASHUTOSH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Ashutosh.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
