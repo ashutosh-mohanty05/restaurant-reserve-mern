51c69dba84911208b831189c342bebfcb06ccea0
Ashutosh Mohanty <94921263+ashutosh-mohanty05@users.noreply.github.com>
2024-06-10 18:04:42 +0530

    commit

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
